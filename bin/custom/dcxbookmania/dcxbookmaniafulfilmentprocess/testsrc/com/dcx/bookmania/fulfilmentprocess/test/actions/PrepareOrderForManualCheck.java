0b99a9b676f1bb5c2b53cea3071a1e5ca3e8351f
SubrataHalder2023 <subrata.halder@capgemini.com>
2023-07-20 19:15:06 +0530

    fixing minor issue

---
+++ bin/custom/dcxbookmania/dcxbookmaniafulfilmentprocess/testsrc/com/dcx/bookmania/fulfilmentprocess/test/actions/PrepareOrderForManualCheck.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.dcx.bookmania.fulfilmentprocess.test.actions;
+
+/**
+ * Test counterpart for
+ * {@link com.dcx.bookmania.fulfilmentprocess.actions.order.PrepareOrderForManualCheckAction}
+ */
+public class PrepareOrderForManualCheck extends TestActionTemp
+{
+	//EMPTY
+}
---
+++ bin/custom/dcxbookmania/dcxbookmaniainitialdata/src/com/dcx/bookmania/initialdata/constants/DcxbookmaniaInitialDataConstants.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.dcx.bookmania.initialdata.constants;
+
+/**
+ * Global class for all DcxbookmaniaInitialData constants.
+ */
+public final class DcxbookmaniaInitialDataConstants extends GeneratedDcxbookmaniaInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "dcxbookmaniainitialdata";
+
+	private DcxbookmaniaInitialDataConstants()
+	{
+		//empty
+	}
+}
