2345d08f9982fa7a3b1cdb1f7fea0a9176a3e884
SubrataHalder2023 <subrata.halder@capgemini.com>
2023-09-05 16:00:53 +0530

    occ added and many issues fixed with impex

---
+++ bin/custom/dcxbookmaniaocc/src/com/dcx/bookmania/constants/DcxbookmaniaoccConstants.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.dcx.bookmania.constants;
+
+public class DcxbookmaniaoccConstants extends GeneratedDcxbookmaniaoccConstants
+{
+	public static final String EXTENSIONNAME = "dcxbookmaniaocc";
+
+	private DcxbookmaniaoccConstants()
+	{
+		//empty
+	}
+}
+
